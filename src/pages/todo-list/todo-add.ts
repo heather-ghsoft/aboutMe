0ebdf76023a9c52d1a6ee8acb27df4d937e77cda
zuhern <eyajiya@gmail.com>
2017-02-13 19:58:57 +0900

    todo-add

---
+++ src/pages/todo-list/todo-add.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { ViewController, NavParams } from 'ionic-angular';
+import { ViewService } from '../../services/view/view.service';
+
+@Component({
+  selector: 'page-todo-add',
+  templateUrl: 'todo-add.html'
+})
+export class TodoAddModal {
+
+  todo = {};
+
+  constructor(
+    private navParams: NavParams,
+    private viewCtrl: ViewController
+  ) {
+
+    this.todo = {
+      title: '',
+      desc: ''
+    }
+  }
+
+  saveTodo() {
+    let data = this.todo;
+    this.dismiss(data);
+  }
+
+  dismiss(data) {
+    this.viewCtrl.dismiss(data); 
+  }
+
+}
