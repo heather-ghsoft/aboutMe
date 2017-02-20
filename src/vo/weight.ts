af60f0d7bb74719acd13dfb1f5e0ffad34d04835
zuhern <eyajiya@gmail.com>
2017-02-20 19:30:37 +0900

    weight vo 추가

---
+++ src/vo/weight.ts
@@ -0,0 +1,14 @@
+export class Weight {
+  constructor(
+    public _id: string,
+    public date: string,
+    public time: string,
+    public value: string,
+    public order: number,
+    public createdAt: number,
+    public lastModifiedAt: number,
+    public dateTime: number
+  ){
+
+  }
+}
